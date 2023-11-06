1fa18f22bc1b47121533d249ef2dcc378cc66378
Peter Koller <peter.koller@molitio.com>
2023-11-06 06:09:52 +0100

    chore(DB-9): fixed local dev execution

--- packages/dashing-bumblebee-resource-db/sql_utils/initialise_db.sql
+++
@@ -1,5 +0,0 @@
--- Terminate all existing connections to the 'default' database.
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'default';
-
--- Rename the database.
-ALTER DATABASE "default" RENAME TO "resource-db";
--- packages/dashing-bumblebee-resource-db/sql_utils/inititialise_db/initialise_db.sh
+++ packages/dashing-bumblebee-resource-db/sql_utils/inititialise_db/initialise_db.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Source the .env file
-source ../../../.env
+ source ./scripts/.env
 
 # Connect to PostgreSQL using environment variables and execute the renaming SQL commands
-# psql -h $PG_DATABASE_ENDPOINT -U $PG_DATABASE_USER -d $PG_DATABASE_NAME -W $PG_DATABASE_SECRET -a -f /initialise_db.sql
-
 PGPASSWORD=$POSTGRES_PASSWORD psql -h $PG_DATABASE_ENDPOINT -U $PG_DATABASE_USER -d $PG_DATABASE_NAME -a -f /initialise_db.sql
